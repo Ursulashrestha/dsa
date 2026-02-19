709fcc721988c3fa38abae9d2140bf31d89dc883
Ursula Shrestha <scrappermoon1999@gmail.com>
2026-02-19 21:35:37 +0100

    added optimized solution for easy array & hashing problem

--- arrays_hashing/easy/valid_anagrams.py
+++ arrays_hashing/easy/valid_anagrams.py
@@ -56,3 +56,15 @@ def isAnagaram(s,t):
 # Space Complexity = O(1)  -- Stores count_s and count_t and the size of the alphabet is fixed 
 
 
+#Optimized loop
+def validAnagram(self,s:str,t:str):
+    if len(s)!=len(t):
+        return False
+    
+    count_s={}
+    count_t={}
+
+    for i in range (len(s)):
+        count_s[s[i]]= 1+ count_s.get(s[i],0)
+        count_t[t[i]]= 1+ count_t.get(t[i],0)
+    return count_s == count_t
