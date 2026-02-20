2cc923e536bee4a7a2b760aeecc093a8c72f1d30
Ursula Shrestha <scrappermoon1999@gmail.com>
2026-02-20 21:20:02 +0100

    solved array and hashing easy question with optimized solution

--- arrays_hashing/easy/valid_anagrams.py
+++ arrays_hashing/easy/valid_anagrams.py
@@ -61,10 +61,25 @@ def validAnagram(self,s:str,t:str):
     if len(s)!=len(t):
         return False
     
-    count_s={}
-    count_t={}
+    count_s, count_t={},{}
 
     for i in range (len(s)):
         count_s[s[i]]= 1+ count_s.get(s[i],0)
         count_t[t[i]]= 1+ count_t.get(t[i],0)
     return count_s == count_t
+
+#using fixed sized array
+def isAnagaram(self,s:str,t:str)-> bool:
+    if len(s)!= len(t):
+        return False
+    
+    count = [0]* 26 #frequency array of size 26
+    #ord('a')= 97
+    for i in range(len(s)):
+        count[ord(s[i])-ord('a')]+=1 #incrementing counts for characters in the first string
+        count[ord(t[i])-ord('a')]-=1 # decrementing for the second. If the strings are anagrams, all counts cancel out and remain zero.
+    
+    for val in count:
+        if val != 0:
+            return False
+    return True
